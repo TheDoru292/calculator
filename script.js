63c4c1e6a74e6fa751d6e56beb201ecf582262c9
Doru <thedoruv2@gmail.com>
2022-01-12 18:39:13 +0100

    Made some functions to work

--- script.js
+++ script.js
@@ -18,7 +18,31 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(operator, num1, num2) {
+function operate(operator, num) {
 
 }
 
+const numbers = document.querySelectorAll(".numbers");
+const clear = document.querySelector(".clear");
+const deleteButton = document.querySelector(".delete");
+
+numbers.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if(displayValue === 0) {
+            displayValue = e.target.textContent;
+        } else{
+            displayValue += e.target.textContent;
+        }
+        result.textContent = displayValue;
+    });
+});
+
+clear.addEventListener("click", (e) => {
+    displayValue = 0;
+    result.textContent = displayValue;
+});
+
+deleteButton.addEventListener("click", (e) => {
+    displayValue = Math.floor(displayValue / 10);
+    result.textContent = displayValue;
+});
